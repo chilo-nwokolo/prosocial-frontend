ba47ed53adaac239b9a0b0b640dcd7ba73fa0a4e
Chilo <chilo.nwokolo@gmail.com>
2023-10-28 11:42:34 +0100

    add sidebar

--- src/components/General/NavBar.tsx
+++ src/components/General/NavBar.tsx
@@ -1,10 +1,13 @@
+'use client';
 import ProfilePicture from '@/components/General/ProfilePicture';
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerOverlay, Flex, useDisclosure } from '@chakra-ui/react';
 import { RiMenu2Fill } from 'react-icons/ri';
 
 export default function NavBar() {
-  return (
-    <Flex
+	const { isOpen, onOpen, onClose } = useDisclosure()
+	return (
+		<>
+			<Flex
 				border="1px solid"
 				borderColor="gray.300"
 				justifyContent="space-between"
@@ -19,10 +22,22 @@ export default function NavBar() {
 				zIndex="modal"
 				alignItems="center"
 			>
-				<Button size="lg" px="3" variant="outline">
-					<RiMenu2Fill style={{ fontSize: "24px" }} />
+				<Button size="lg" px="3" variant="outline" onClick={onOpen}>
+					<RiMenu2Fill style={{ fontSize: '24px' }} />
 				</Button>
 				<ProfilePicture />
 			</Flex>
-  )
+			<Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+				<DrawerCloseButton />
+          <DrawerBody>
+            <p>Home</p>
+            <p>Profile</p>
+            <p>Contact</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+		</>
+	);
 }
