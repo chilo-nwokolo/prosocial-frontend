ccc36d6fd572745f11b0861274b66c9498739017
Chilo <chilo.nwokolo@gmail.com>
2023-11-20 13:12:24 +0100

    ft: add admin login page

--- src/app/pro-admin/layout.tsx
+++ src/app/pro-admin/layout.tsx
@@ -1,10 +1,9 @@
 'use client';
-import NavBar from '@/components/General/NavBar';
 import { Box, usePrevious } from '@chakra-ui/react';
 import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 
-export default function ProLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
 	const previousPathname = usePrevious(pathname);
 
@@ -18,21 +17,8 @@ export default function ProLayout({ children }: { children: React.ReactNode }) {
 		}
 	}, [pathname, previousPathname]);
 	return (
-		<Box as="main" maxWidth="lg" minWidth="250px" mx="auto">
-			<NavBar />
-			<Box
-				as="section"
-				border="1px solid"
-				borderColor="gray.300"
-				overflowY="auto"
-				px="6"
-				pt="10"
-				pb="4"
-				mt="14"
-				minH="100vh"
-			>
-				{children}
-			</Box>
+		<Box as="main">
+			{children}
 		</Box>
 	);
 }
