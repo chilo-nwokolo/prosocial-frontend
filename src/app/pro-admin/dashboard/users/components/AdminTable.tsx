4629fcd6e7481ec948f7f5beade58ac5bdd32a59
Chilo <chilo@getrova.com>
2024-06-09 14:33:22 +0100

    update admin table position

--- src/app/pro-admin/dashboard/users/components/AdminTable.tsx
+++ src/app/pro-admin/dashboard/users/components/AdminTable.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {
   Box,
+  Button,
   Table,
   TableContainer,
   Tbody,
@@ -28,35 +29,46 @@ export default function AdminTable({
           <Thead>
             {table.getHeaderGroups()?.map((headerGroup) => (
               <Tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <Th
-                    key={header.id}
-                    colSpan={header.colSpan}
-                    border="1px solid"
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
+                <>
+                  <Th border="1px solid" fontSize="lg" fontWeight="regular">
+                    Action
                   </Th>
-                ))}
+                  {headerGroup.headers.map((header) => (
+                    <Th
+                      key={header.id}
+                      colSpan={header.colSpan}
+                      border="1px solid"
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </Th>
+                  ))}
+                </>
               </Tr>
             ))}
           </Thead>
           <Tbody>
             {table.getRowModel().rows.map((row) => (
-              <Tr
-                key={row.id}
-                cursor="pointer"
-                onClick={() => handleClick(row.original)}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <Td key={cell.id} border="1px solid">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              <Tr key={row.id} cursor="pointer">
+                <>
+                  <Td border="1px solid">
+                    <Button onClick={() => handleClick(row.original)}>
+                      View Details
+                    </Button>
                   </Td>
-                ))}
+                  {row.getVisibleCells().map((cell) => (
+                    <Td key={cell.id} border="1px solid">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </Td>
+                  ))}
+                </>
               </Tr>
             ))}
           </Tbody>
